e26bddb57c353019e145df41c5068b3f0607c1cc
Almeida154 <davidalmeida154of@gmail.com>
2022-10-18 16:51:09 -0300

    devices responsiveness

--- src/components/Devices/index.jsx
+++ src/components/Devices/index.jsx
@@ -1,4 +1,12 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import Button from '../Button';
 import Title from '../Title';
+
+import defaultDevice from '../../assets/images/default-device.png';
+
+import 'swiper/css';
+import 'swiper/css/autoplay';
 
 import './styles.scss';
 
@@ -7,7 +15,67 @@ const Devices = () => {
     <section className="devices">
       <Title text="Dispositivos" subtext="O que é usado?" />
 
-      <div className="device__container container">devices</div>
+      <div className="devices__container container">
+        <div className="carousel">
+          <Swiper
+            slidesPerView={2.5}
+            spaceBetween={32}
+            autoplay
+            speed={500}
+            draggable
+            breakpoints={{
+              1024: {
+                slidesPerView: 3.5,
+              },
+              768: {
+                slidesPerView: 2.5,
+              },
+              564: {
+                slidesPerView: 1.5,
+              },
+            }}
+          >
+            <SwiperSlide>
+              <img src={defaultDevice} />
+            </SwiperSlide>
+            <SwiperSlide>
+              {' '}
+              <img src={defaultDevice} />
+            </SwiperSlide>
+            <SwiperSlide>
+              {' '}
+              <img src={defaultDevice} />
+            </SwiperSlide>
+            <SwiperSlide>
+              {' '}
+              <img src={defaultDevice} />
+            </SwiperSlide>
+            <SwiperSlide>
+              {' '}
+              <img src={defaultDevice} />
+            </SwiperSlide>
+          </Swiper>
+        </div>
+
+        <div className="content">
+          <p>
+            Os sensores são fornecidos pela Steck. Consulte se seu{' '}
+            <a className="highlight" style={{ textDecoration: 'underline' }}>
+              plano
+            </a>{' '}
+            inclui determinado dispositivo.
+          </p>
+
+          <a href="https://www.steck.com.br/" target="_blank">
+            <Button
+              text="Conhecer"
+              isSolid
+              hasIcon
+              customStyle={{ marginTop: 40, fontSize: '2rem' }}
+            />
+          </a>
+        </div>
+      </div>
     </section>
   );
 };
